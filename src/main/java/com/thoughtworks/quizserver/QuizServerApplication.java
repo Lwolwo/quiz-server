56ddbd0a04136c6640b7600ea4f4f43ac93de6f9
Lwolwo <zuihouyiye0704@126.com>
2020-08-14 14:10:33 +0800

    init

---
+++ src/main/java/com/thoughtworks/quizserver/QuizServerApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.quizserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuizServerApplication.class, args);
+    }
+
+}
